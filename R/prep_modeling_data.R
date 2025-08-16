b79c573280c148545084f43d1696f417be3740c4
timop26 <timopew@gmail.com>
2025-08-16 15:50:20 -0400

    Remove unnecessary code

--- R/prep_modeling_data.R
+++ R/prep_modeling_data.R
@@ -6,7 +6,11 @@ source("~/triple-double-predictor/R/add_features.R")
 for (i in 2025:2022) {
   start <- Sys.time()
   in_game_player_stats <- create_in_game_player_stats(season=i)
-  write.csv(in_game_player_stats, file=paste0("~/triple-double-predictor/data/in_game_player_stats_", i, ".csv"), row.names=FALSE)
+  write.csv(
+    in_game_player_stats, 
+    file=paste0("~/triple-double-predictor/data/in_game_player_stats_", i, ".csv"), 
+    row.names=FALSE
+  )
   end <- Sys.time()
   print(paste("Took", round(difftime(end, start, units="mins")), "minutes"))
 }
@@ -17,7 +21,10 @@ for (i in 2025:2022) {
   in_game_player_stats <- join_game_and_score(in_game_player_stats, season=i)
   in_game_player_stats <- in_game_player_stats %>%
     filter(game_play_number %% 4 == 1, tri_dbl == 0)
-  write.csv(in_game_player_stats, file=paste0("~/triple-double-predictor/data/modeling_data_", i, ".csv"), row.names=FALSE)
+  write.csv(
+    in_game_player_stats, 
+    file=paste0("~/triple-double-predictor/data/modeling_data_", i, ".csv"), 
+    row.names=FALSE
+  )
 }
-
 
